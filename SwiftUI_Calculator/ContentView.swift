b6e475f9393a57ffa10bf95cd6af150e72943f98
Arjun Suthaharan <arjunsutha@gmail.com>
2023-09-04 20:36:56 -0400

    Initial Commit

---
+++ SwiftUI_Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Calculator
+//
+//  Created by Arjun Suthaharan on 2023-09-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Calculator/SwiftUI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CalculatorApp.swift
+//  SwiftUI_Calculator
+//
+//  Created by Arjun Suthaharan on 2023-09-04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
