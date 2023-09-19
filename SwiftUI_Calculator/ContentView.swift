583f9dcbc28eb4ba605ba186ab3736083a3a4678
Arjun Suthaharan <arjunsutha@gmail.com>
2023-09-19 19:30:36 -0400

    Implemented logic for pressing number values on calculator, saving inputted values to memory, and clearing inputted values back to '0'

--- SwiftUI_Calculator/ContentView.swift
+++ SwiftUI_Calculator/ContentView.swift
@@ -4,7 +4,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var numberOne : String = "0"
+    @State var number : String = "0"
     @State var operand : NSInteger = 0
     @State var resultant : Double = 0.0
     @State var firstNum : NSInteger = 0
@@ -25,7 +25,7 @@ struct ContentView: View {
                 .font(.system(size: 30))
             
             
-            Text("0")
+            Text(number)
                 .padding()
                 .font(.system(size: 30))
                 .frame(width: 300, height: 30, alignment: .trailing)
@@ -33,68 +33,71 @@ struct ContentView: View {
             HStack(spacing: 25){
                 
                 Button("7"){
-                    
+                    self.numPressed(pressedValue: "7")
                 }.buttonStyle(CalculatorButtonStyle())
                 
                 Button("8"){
-                    
+                    self.numPressed(pressedValue: "8")
                 }.buttonStyle(CalculatorButtonStyle())
                 
                 Button("9"){
-                    
+                    self.numPressed(pressedValue: "9")
                 }.buttonStyle(CalculatorButtonStyle())
                 
                 Button("+"){
-                    
+                    self.operand = self.PLUS
+                    self.saveFirstNum()
                 }.buttonStyle(CalculatorButtonStyle())
             
             }
             HStack(spacing: 25){
                 
                 Button("4"){
-                    
+                    self.numPressed(pressedValue: "4")
                 }.buttonStyle(CalculatorButtonStyle())
                 
                 Button("5"){
-                    
+                    self.numPressed(pressedValue: "5")
                 }.buttonStyle(CalculatorButtonStyle())
                 
                 Button("6"){
-                    
+                    self.numPressed(pressedValue: "6")
                 }.buttonStyle(CalculatorButtonStyle())
                 
                 Button("-"){
-                    
+                    self.operand = self.MINUS
+                    self.saveFirstNum()
                 }.buttonStyle(CalculatorButtonStyle())
             
             }
             HStack(spacing: 25){
                 
                 Button("1"){
-                    
+                    self.numPressed(pressedValue: "1")
                 }.buttonStyle(CalculatorButtonStyle())
                 
                 Button("2"){
-                    
+                    self.numPressed(pressedValue: "2")
                 }.buttonStyle(CalculatorButtonStyle())
                 
                 Button("3"){
-                    
+                    self.numPressed(pressedValue: "3")
                 }.buttonStyle(CalculatorButtonStyle())
                 
                 Button("X"){
-                    
+                    self.operand = self.MULTI
+                    self.saveFirstNum()
                 }.buttonStyle(CalculatorButtonStyle())
             
             }
             HStack(spacing: 25){
                 
                 Button("C"){
-                    
+                    self.clearNumValue()
                 }.buttonStyle(CalculatorButtonStyle())
                 
                 Button("0"){
-                    
+                    self.numPressed(pressedValue: "0")
                 }.buttonStyle(CalculatorButtonStyle())
                 
                 Button("="){
@@ -102,12 +105,35 @@ struct ContentView: View {
                 }.buttonStyle(CalculatorButtonStyle())
                 
                 Button("%"){
-                    
+                    self.operand = self.DIV
+                    self.saveFirstNum()
                 }.buttonStyle(CalculatorButtonStyle())
             
             }
         }
+        //.frame(width: 300, height: 500, alignment: .top)
     }
+    
+    func numPressed(pressedValue : String){
+        
+        if(self.number == "0"){
+            self.number = pressedValue
+        }
+        else{
+            self.number += pressedValue
+        }
+        
+    }
+    
+    func saveFirstNum(){
+        firstNum = Int(number)!
+        number = "0"
+    }
+    
+    func clearNumValue(){
+        number = "0"
+    }
+    
 }
 
 struct CalculatorButtonStyle: ButtonStyle{
